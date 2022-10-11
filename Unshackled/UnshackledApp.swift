cda2811ce1a8b8bfa9d7ed1f980ac5b3b28bcae9
Bradlee King <Kingofbrad@gmail.com>
2022-10-11 14:30:30 +0100

    Initial Commit

---
+++ Unshackled/UnshackledApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnshackledApp.swift
+//  Unshackled
+//
+//  Created by Bradlee King on 11/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnshackledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
